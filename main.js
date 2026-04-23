fea959823115552010e6458bf46195d74d2ac744
Jony Mia <ajonymia321@gmail.com>
2026-04-23 22:19:42 +0600

    update

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let progress=document.getElementById('progres');
+let front=document.getElementById('front');
+let back=document.getElementById('back');
 let round=document.querySelector('.img');
 let act=document.getElementById('action');
 let music= new Audio('/file/Suzume no Tojimari『Suzume』Theme Song.mp3');
-
+let forward = 10
+let rewind = 10
 act.addEventListener('click',()=>{
   if (act.classList.contains('fa-play')) {
     act.classList.remove('fa-play');
@@ -14,11 +17,9 @@ act.addEventListener('click',()=>{
     act.classList.add('fa-play');
     music.pause();
     round.classList.remove('active');
-
   }
 })
 music.onloadedmetadata=()=>{
-
     progress.max = music.duration;
     progress.value = music.currentTime;
 }
@@ -26,9 +27,14 @@ music.onloadedmetadata=()=>{
 if (music.play()) {
   setInterval(()=>{
         progress.value = music.currentTime;
-
   },1000)
 }
 progress.onchange=()=>{
   music.currentTime=progress.value
 }
+back.addEventListener('click',()=>{
+  music.currentTime-=rewind 
+})
+front.addEventListener('click',()=>{
+  music.currentTime+=forward 
+})
