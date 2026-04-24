a81357086a0ccf657bbe97305becfc10e99dcbfd
Jony-Mia <ajonymia321@gmail.com>
2026-04-24 12:10:55 +0600

    rotate disk added

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let back = document.getElementById("back");
 let forward = document.getElementById("forward")
 let progress = document.getElementById("progress");
 let music = new Audio("files/song-item.mpeg")
+let disk = document.getElementById("disk");
 
 
 play.onclick=()=>{
@@ -10,21 +11,24 @@ play.onclick=()=>{
         music.play();
         play.classList.remove("fa-play");
         play.classList.add("fa-pause");
+        disk.classList.add("rotate");
+
     } else {
         music.pause();
         play.classList.remove("fa-pause");
         play.classList.add("fa-play");
-    }
-    if (music.play()) {
-        setInterval(()=>{
-            progress.value= music.currentTime
-        }, 1000);
+        disk.classList.remove("rotate");
     }
 }
 
 music.onloadedmetadata=()=>{
     progress.max= music.duration;
     progress.value = music.currentTime;
+}
+if (music.play()) {
+    setInterval(()=>{
+        progress.value= music.currentTime
+    }, 1000);
 }
 progress.onchange=()=>{
     music.currentTime = progress.value
