112730a7329c8fe3179310e51f35469776b3922a
Jony-Mia <ajonymia321@gmail.com>
2026-04-24 11:38:10 +0600

    project completed

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-let progress=document.getElementById('progres');
-let front=document.getElementById('front');
-let back=document.getElementById('back');
-let round=document.querySelector('.img');
-let act=document.getElementById('action');
-let music= new Audio('/file/suzume.mp3');
-let forward = 10
-let rewind = 10
-act.addEventListener('click',()=>{
-  if (act.classList.contains('fa-play')) {
-    act.classList.remove('fa-play');
-    act.classList.add('fa-pause');
-    music.play();
-    round.classList.add('active');
-  } else {
-    act.classList.remove('fa-pause');
-    act.classList.add('fa-play');
-    music.pause();
-    round.classList.remove('active');
-  }
-})
-music.onloadedmetadata=()=>{
-    progress.max = music.duration;
-    progress.value = music.currentTime;
+let play = document.getElementById("play");
+let back = document.getElementById("back");
+let forward = document.getElementById("forward")
+let progress = document.getElementById("progress");
+let music = new Audio("files/song-item.mpeg")
+
+
+play.onclick=()=>{
+    if (play.classList.contains("fa-play")) {
+        music.play();
+        play.classList.remove("fa-play");
+        play.classList.add("fa-pause");
+    } else {
+        music.pause();
+        play.classList.remove("fa-pause");
+        play.classList.add("fa-play");
+    }
+    if (music.play()) {
+        setInterval(()=>{
+            progress.value= music.currentTime
+        }, 1000);
+    }
 }
 
-if (music.play()) {
-  setInterval(()=>{
-        progress.value = music.currentTime;
-  },1000)
+music.onloadedmetadata=()=>{
+    progress.max= music.duration;
+    progress.value = music.currentTime;
 }
 progress.onchange=()=>{
-  music.currentTime=progress.value
+    music.currentTime = progress.value
 }
-back.addEventListener('click',()=>{
-  music.currentTime-=rewind 
-})
-front.addEventListener('click',()=>{
-  music.currentTime+=forward 
-})
+
+back.onclick=()=>{
+    music.currentTime -=10;
+    
+}
+forward.onclick=()=>{
+    music.currentTime+= 10;
+}
